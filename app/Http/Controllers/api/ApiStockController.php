3bf49251541580034452794a9904b16f6ea911a4
MrQwerty9 <sholihin.ok1998@gmail.com>
2020-02-28 11:25:02 +0700

    stock fix

--- app/Http/Controllers/api/ApiStockController.php
+++ app/Http/Controllers/api/ApiStockController.php
@@ -12,34 +12,29 @@ class ApiStockController extends Controller
     {
 
     	$stock = \App\Stock::select('products.name AS product', 'products.price', 'stock.quantity')->join('products', 'products.id', '=', 'stock.id_product')->get();
-
-        $meta = [
-           'code' => Response::HTTP_OK, 
-           'message' => 'Success'
-       ];
-
-
-       return response()->json(['meta' => $meta, 'data' => $stock]);
-   }
-
-   function update(Request $request)
-   {
-     $currentStock = \App\Stock::where('id', $request->product_id)->first();
-     $updatedStock = $currentStock['quantity'] - $request->number_of_fetches;
-     $stock = \App\Stock::where('id', $request->product_id)->update(['quantity' => $updatedStock]);
-
-     if ($stock) {
         $meta = [
             'code' => Response::HTTP_OK, 
             'message' => 'Success'
         ];
+        return response()->json(['meta' => $meta, 'data' => $stock]);
+    }
 
-        return response()->json(['meta' => $meta);    
-    }else{
-        return response()->json([
-            'code' => Response::HTTP_METHOD_FAILURE, 
-            'message' => 'Gagal disimpan'
-        ]);
-
+    function update(Request $request) 
+    { 
+        $currentStock = \App\Stock::where('id', $request->product_id)->first(); 
+        $updatedStock = $currentStock['quantity'] - $request->number_of_fetches; 
+        $stock = \App\Stock::where('id', $request->product_id)->update(['quantity' => $updatedStock]); 
+        if ($stock) { 
+            $meta = [ 
+                'code' => Response::HTTP_OK, 
+                'message' => 'Success' 
+            ]; 
+            return response()->json(['meta' => $meta]); 
+        }else{ 
+            return response()->json([ 
+                'code' => Response::HTTP_METHOD_FAILURE, 
+                'message' => 'Gagal disimpan' 
+            ]); 
+        } 
     }
 }

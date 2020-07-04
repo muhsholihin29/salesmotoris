1bb1e57f1ac2acfe58ea1f5be01137f416544581
DESKTOP-TJ3RLOR\sholi <sholihin.ok1998@gmail.com>
2020-07-04 11:20:50 +0700

    fix store

--- app/Store.php
+++ app/Store.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     protected $table = 'stores';
-    protected $fillable = ['name','address','coordinate'];
+    protected $fillable = ['name','address','coordinate','status'];
 }
+ 
